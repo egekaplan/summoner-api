3dfe2b18cfce6c82fa9b9e411a6271d771adad3e
Ege Kaplan <ege.kaplan@ozu.edu.tr>
2022-12-07 14:17:35 +0300

    first commit

---
+++ riot_app_tracker/src/main/java/ege/App.java
@@ -0,0 +1,29 @@
+package ege;
+
+import java.util.*;
+import net.rithms.riot.api.ApiConfig;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.match.dto.MatchReference;
+import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
+import net.rithms.riot.api.endpoints.match.dto.MatchList;
+import net.rithms.riot.constant.Platform;
+
+public class App 
+{
+    private static Platform TR = Platform.TR;
+    private static final String RIOT_KEY = "RGAPI-1070f1b9-f7bf-44c4-95f8-0a659132254f";
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println("Enter your summoner name; ");
+        String summonerName = new Scanner(System.in).nextLine();
+        
+        ApiConfig config = new ApiConfig().setKey(RIOT_KEY);
+		RiotApi api = new RiotApi(config);
+
+		Summoner summoner = api.getSummonerByName(TR, summonerName);
+
+		System.out.println("Summoner Name: " + summoner.getName());
+		System.out.println("Summoner Level: " + summoner.getSummonerLevel());           
+    }
+}
